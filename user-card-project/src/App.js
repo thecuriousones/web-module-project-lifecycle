718a4ddc3d0fe634b1274a749ec9c90f51f6a2ca
thecuriousones <tamaraleonard46@gmail.com>
2021-06-06 17:22:00 -0400

    user is displayed and styled working on follower components

--- user-card-project/src/App.js
+++ user-card-project/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
+import User from './components/User';
 
 
 
@@ -12,11 +13,37 @@ class App extends React.Component{
     }
   }
 
+  componentDidMount() {
+    console.log("App CDM")
+    axios.get('https://api.github.com/users/thecuriousones')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          ...this.state,
+          user: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  // fetchFollowers = () => {
+  //   console.log('is this working?')
+  //   axios.get('https://api.github.com/users/thecuriousones/followers')
+  //     .then(res => {
+  //       console.log(res);
+  //       this.setState({
+  //         ...this.state,
+  //         user: res.data
+  //       });
+  //     })
+  //     .catch(err => console.log(err));
+  // }
+
 
   render() {
     return (
-      <div className="App">
-      <div>INCOMING PROJECT</div>
+      <div className="flex w-full flex-row flex-wrap justify-start bg-blue-50">
+      <User userObject={this.state.user} />
     </div>
     )
   }
